ad1630e722346c72ce8c885f6f6d90b2ed0a2819
Trogious <trog@swmud.net>
2018-07-28 18:30:30 +0200

    ExpiresIn change to less than a week

--- jar_lambda_notify.py
+++ jar_lambda_notify.py
@@ -1,5 +1,4 @@
 import boto3
-from botocore.client import Config
 import os
 
 
@@ -7,9 +6,9 @@ def handler(event, context):
     sns = boto3.client('sns')
     key = event['Records'][0]['s3']['object']['key']
     bucket = event['Records'][0]['s3']['bucket']['name']
-    s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
-    SIXTY_ONE_DAYS_IN_SECONDS = 5270400
-    url = s3.generate_presigned_url(ClientMethod='get_object', Params={'Bucket': bucket, 'Key': key}, ExpiresIn=SIXTY_ONE_DAYS_IN_SECONDS)
+    s3 = boto3.client('s3')
+    ONE_WEEK_IN_SECONDS = 604800
+    url = s3.generate_presigned_url(ClientMethod='get_object', Params={'Bucket': bucket, 'Key': key}, ExpiresIn=ONE_WEEK_IN_SECONDS-1)
     message = 'Jar execution result is now ready: ' + key + '.\n\nTo download, click here: ' + url + '\n\n\n'
     sns.publish(TopicArn=os.getenv('JAR_LAMBDA_NOTIFY_SNS_ARN'), Subject='JAR execution complete', Message=message)
     return key
