f2ee29cdfd06eff045c46f4278526ad20681318c
Trogious <trog@swmud.net>
2018-07-28 17:23:38 +0200

    lambda notify presign fixes, lambda schedule increased timeout

--- jar_lambda_notify.py
+++ jar_lambda_notify.py
@@ -8,7 +8,8 @@ def handler(event, context):
     key = event['Records'][0]['s3']['object']['key']
     bucket = event['Records'][0]['s3']['bucket']['name']
     s3 = boto3.client('s3', config=Config(signature_version='s3v4'))
-    url = s3.generate_presigned_url(ClientMethod='get_object', Params={'Bucket': bucket, 'Key': key})
+    SIXTY_ONE_DAYS_IN_SECONDS = 5270400
+    url = s3.generate_presigned_url(ClientMethod='get_object', Params={'Bucket': bucket, 'Key': key}, ExpiresIn=SIXTY_ONE_DAYS_IN_SECONDS)
     message = 'Jar execution result is now ready: ' + key + '.\n\nTo download, click here: ' + url + '\n\n\n'
     sns.publish(TopicArn=os.getenv('JAR_LAMBDA_NOTIFY_SNS_ARN'), Subject='JAR execution complete', Message=message)
     return key
